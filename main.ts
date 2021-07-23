34be0c75dcfa2b801d8f45329948731a6abdc7c0
OolongJohnson <83778844+OolongJohnson@users.noreply.github.com>
2021-07-23 16:56:42 +0200

    .hex uploaded

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Yes)
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.No)
+})
+basic.showIcon(IconNames.Heart)
